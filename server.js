88bbc0cf798a654b1671c80d879a68515008eaaa
Mona Lisa <soaresmaria181@yahoo.com>
2021-09-11 02:24:13 -0400

    added prompts to addEmployee function

--- server.js
+++ server.js
@@ -78,6 +78,37 @@ function viewDepartments() {
     })
 }
 
+function addEmployee() {
+    inquirer.prompt([{
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?"
+        },
+        {
+            type: "number",
+            name: "roleId",
+            message: "What is the employee's role ID"
+        },
+        {
+            type: "number",
+            name: "managerId",
+            message: "What is the employee's manager's ID?"
+        }
+    ]).then(function(res) {
+        connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', 
+        [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+            if (err) throw err;
+            console.table("Successfully Inserted");
+            askQuestions();
+        })
+    })
+}
+
 
         askQuestions();
     
