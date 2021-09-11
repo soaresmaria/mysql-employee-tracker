eaee34b970dcd6da300845734f57d8c9ac77ca04
Mona Lisa <soaresmaria181@yahoo.com>
2021-09-11 01:44:52 -0400

    created prompts

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
 
-
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -16,7 +15,54 @@ connection.connect(function (err) {
     askQuestions();
 });
 
-function askQuestions() {};
+function askQuestions() {
+    inquirer.prompt({
+        message: "what would you like to do?",
+        type: "list",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "Add an employee",
+            "Add a departmentt",
+            "Add a role",
+            "Update employee role",
+            "QUIT"
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "View all employees":
+                viewEmployees()
+                break;
+
+            case "View all departments":
+                viewDepartments()
+                break;
+
+            case "Add an employee":
+                addEmployee()
+                break;
+
+            case "Add a department":
+                addDepartment()
+                break;
+
+            case "Add a role":
+                addRole()
+                break;
+
+            case "Update employee role":
+                updateEmployeeRole();
+                break;
+
+            default:
+                connection.end()
+                break;
+        }
+    })
+}
 
 
-askQuestions();
+        askQuestions();
+    
